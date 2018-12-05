8d2657ac8fb7d36f585ff286513b1a6de2dcad39
Jack Foltz <jack@foltz.io>
2018-12-05 16:50:49 -0500

    Add property inference based on files present

--- lib/parse.py
+++ lib/parse.py
@@ -1,4 +1,5 @@
 import yaml
+from pathlib import Path
 
 class Package:
     def __init__(self, obj):
@@ -10,8 +11,39 @@ class Package:
         else:
             self.name = obj
             self.source = 'core'
+
+        self.update_from_files()
+        
     def __repr__(self):
         return self.name
+
+    def update_from_files(self):
+        if self.source == 'none':
+            self.source = None
+        
+        default_config = Path('lain/.config/' + self.name)
+        if hasattr(self, 'config'):
+            self.config = Path('lain/' + self.config)
+        elif default_config.is_dir():
+            self.config = default_config
+        else:
+            self.config = None
+        
+        default_script = Path('scripts/' + self.name + '.fish')
+        if hasattr(self, 'script'):
+            self.script = Path('scripts/' + self.script)
+        elif default_script.is_file():
+            self.script = default_script
+        else:
+            self.script = None
+
+        default_unit = Path('lain/.config/systemd/user/' + self.name + '.service')
+        if hasattr(self, 'userunits'):
+            self.userunits = list(map(lambda unit: Path('lain/.config/systemd/user/' + unit), self.userunits))
+        elif default_unit.is_file():
+            self.userunits = [default_unit]
+        else:
+            self.userunits = None
                 
 class Category:
     def __init__(self, name, packages):
