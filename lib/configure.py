d099c1d1d83027802a85efbc6623698b525b31c2
Jack Foltz <jack@foltz.io>
2018-12-05 16:51:19 -0500

    Add configure stub methods

--- lib/configure.py
+++ lib/configure.py
@@ -1,5 +1,29 @@
+import os
 from subprocess import call
+from pathlib import Path
+
 from lib.parse import Package
 
+def install(package):
+    if not package.source:
+        return
+
+def copy_config(package):
+    if not package.config:
+        return
+
+def run_script(package):
+    if not package.script:
+        return
+
+def enable_units(package):
+    if not package.userunits:
+        return
+
 def configure(package):
-    pass
+    print(vars(package))
+    install(package)
+    copy_config(package)
+    run_script(package)
+    enable_units(package)
+    
