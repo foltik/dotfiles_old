3462211ac761ce43ab899cb9a894a0c2a55ba624
Jack Foltz <jack@foltz.io>
2018-12-12 16:41:51 -0500

    Update fish config

--- lain/.config/fish/config.fish
+++ lain/.config/fish/config.fish
@@ -2,9 +2,6 @@
 set -U fish_greeting
 set -U fish_user_paths ~/.local/bin
 
-# Use all cores for make by default
-set -gx MAKEFLAGS -j(nproc)
-
 # TTY for gpg pinentry
 set -gx GPG_TTY (tty)
 gpg-connect-agent updatestartuptty /bye >/dev/null
@@ -12,14 +9,6 @@ gpg-connect-agent updatestartuptty /bye >/dev/null
 # Load wal colors if installed
 if test -d ~/.cache/wal
     cat ~/.cache/wal/sequences &
-end
-
-# Load autojump if installed
-begin
-    set -l AUTOJUMP_PATH /usr/share/autojump/autojump.fish
-    if test -e $AUTOJUMP_PATH
-        source $AUTOJUMP_PATH
-    end
 end
 
 # Load keymap
