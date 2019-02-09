e4a629db805932069d38971407af63bacf1afefc
Jack Foltz <jack@foltz.io>
2019-02-08 21:14:49 -0500

    More sandbox formatting

--- lain/.emacs.d/init.el
+++ lain/.emacs.d/init.el
@@ -1,5 +1,6 @@
-(package-initialize)
+;; Needs to be commented or else it will be added back
+; (package-initialize)
 
-(message "Initializing emacs...")
+(message "Initializing Electronic Macs...")
 (org-babel-load-file
   (expand-file-name "config.org" user-emacs-directory))
