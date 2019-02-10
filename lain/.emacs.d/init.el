659984a9ad18ae127c79bc71e2f197a87d9cead0
Jack Foltz <jack@foltz.io>
2019-02-10 14:09:23 -0500

    Move sandbox into main config

--- lain/.emacs.d/init.el
+++ lain/.emacs.d/init.el
@@ -1,7 +1,7 @@
 ;; Needs to be commented or else it will be added back
 ; (package-initialize)
 
-(defvar jf-config-file (expand-file-name "sandbox.org" user-emacs-directory))
+(defvar jf-config-file (expand-file-name "config.org" user-emacs-directory))
 (defvar jf-init-file (expand-file-name "init.el" user-emacs-directory))
 (defvar jf-load-path (expand-file-name "lisp/" user-emacs-directory))
 
