4a99b8fc81d423ec2fe277078f62b19c307957e1
Jack Foltz <jack@foltz.io>
2019-02-10 11:52:14 -0500

    Add global paths to init.el

--- lain/.emacs.d/init.el
+++ lain/.emacs.d/init.el
@@ -1,6 +1,9 @@
 ;; Needs to be commented or else it will be added back
 ; (package-initialize)
 
+(defvar jf-config-file (expand-file-name "sandbox.org" user-emacs-directory))
+(defvar jf-init-file (expand-file-name "init.el" user-emacs-directory))
+(defvar jf-load-path (expand-file-name "lisp/" user-emacs-directory))
+
 (message "Initializing Electronic Macs...")
-(org-babel-load-file
-  (expand-file-name "config.org" user-emacs-directory))
+(org-babel-load-file jf-config-file)
